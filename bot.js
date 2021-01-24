6fc0761de16a53d087d62c4fac58dcdc1dd2eb29
José Ramón Rodríguez Hernández <shadeamidlight@gmail.com>
2021-01-24 12:31:35 +0000

    Primer commit

---
+++ bot.js
@@ -0,0 +1,55 @@
+// dotenv, facilita el uso de variables de entorno, para gestionar la key del bot en este caso
+const dotenv = require('dotenv')
+dotenv.config()
+
+const Telegraf = require('telegraf')
+const bot = new Telegraf(process.env.ID_BOT)
+
+let BaseDeDatos = []	//Intentar usar las Sessions de telegraf
+
+
+bot.start((ctx) => {
+	ctx.reply('Holii^^');
+})
+
+bot.help((ctx) => {
+	ctx.reply('Help, its not fun :O');
+})
+
+bot.settings((ctx) => {
+	ctx.reply('setting');
+})
+
+// Se espera que llegue (Persona, Día). Luego ya veremos fecha en ves de día.
+bot.command('add', (ctx) => {
+	ctx.reply('Comando de añadir a la lista de cumples')
+	const textChat = ctx.message.text.split(' ')
+
+	if ( textChat.length != 3 )
+		ctx.reply("Necesito que me digas /add Nombre Dia")
+	else if (BaseDeDatos[textChat[1]])
+		ctx.reply(`Lo siento, ya tenemos registrado a ${textChat[1]} ;.;`)
+	else {
+		BaseDeDatos[textChat[1]] = textChat[2]
+		ctx.reply("Añadido para notificar jeje")
+	}
+
+	//console.log(typeof(ctx.message.text))
+
+})
+
+bot.command('ver', (ctx) => {
+	let a = 'Aca tenemos a:\n'
+
+	for ( i in BaseDeDatos) {
+		a = a +`* ${i}: el día ${BaseDeDatos[i]}\n`
+	}
+
+	ctx.reply(a)
+})
+
+// Lo ejecuta
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
